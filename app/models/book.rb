a57f273b069652ddbabd578cc4ec44579cad07d2
krishnaeverestengineering <krishnamurthy.t@everest.engineering>
2020-02-10 12:32:18 +0530

    added vaidations to new book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+    validates :name, length: {maximum:30}, presence: true
+    validates :author, presence: true
+    validates :price, presence: true
+    validates :desc, presence: true,  length: {maximum:1000}
+    validates :short_desc, presence: true,  length: {maximum:300}
+    validate :price_allowed
+
+    def price_allowed
+        if price.present? && price < 0
+            errors.add(:price, "Invaid price")
+        end
+    end
+    
 end
